92bc9a2dcc282df04bcb47a5dc64a814653265c4
Andrea Cimitan <andrea.cimitan@gmail.com>
2008-03-13 11:04:36 +0000

    Various fixes to murrine_get_parent_bg. Thanks to Benjamin Berg
    2008-03-13  Andrea Cimitan  <andrea.cimitan@gmail.com>
    
    	* src/cairo-support.c: (murrine_get_parent_bg):
    	Various fixes to murrine_get_parent_bg.
    	Thanks to Benjamin Berg
    
    svn path=/trunk/; revision=29

--- src/cairo-support.c
+++ src/cairo-support.c
@@ -242,14 +242,34 @@ murrine_get_parent_bg (const GtkWidget *widget, MurrineRGB *color)
 	GtkStateType state_type;
 	const GtkWidget *parent;
 	GdkColor *gcolor;
+	gboolean stop;
 
 	if (widget == NULL)
 		return;
 
 	parent = widget->parent;
+	stop = FALSE;
+	
+	while (parent && !stop)
+	{
+		stop = FALSE;
 
-	while (parent && GTK_WIDGET_NO_WINDOW (parent) && !((GTK_IS_NOTEBOOK (parent)) || (GTK_IS_TOOLBAR (parent))))
-		parent = parent->parent;
+		stop |= !GTK_WIDGET_NO_WINDOW (parent);
+		stop |= GTK_IS_NOTEBOOK (parent) &&
+		        !gtk_notebook_get_show_tabs (GTK_NOTEBOOK (parent)) &&
+		        gtk_notebook_get_show_border (GTK_NOTEBOOK (parent));
+
+		if (GTK_IS_TOOLBAR (parent))
+		{
+			GtkShadowType shadow = GTK_SHADOW_OUT;
+			gtk_widget_style_get (GTK_WIDGET (parent), "shadow-type", &shadow, NULL);
+			
+			stop |= (shadow != GTK_SHADOW_NONE);
+		}
+
+		if (!stop)
+			parent = parent->parent;
+	}
 
 	if (parent == NULL)
 		return;

2f82e824bbdecfc47c98ebed2efad8c9a72396b2
Andrea Cimitan <andrea.cimitan@gmail.com>
2008-08-27 18:33:41 +0000

    Updated autoge.sh, removed config.h.in now generated by autogen.sh Thanks
    2008-08-27  Andrea Cimitan  <andrea.cimitan@gmail.com>
    
    	* autogen.sh:
    	* src/config.h.in:
    	Updated autoge.sh, removed config.h.in now generated by autogen.sh
    	Thanks to Werner Pantke
    
    svn path=/trunk/; revision=48

--- autogen.sh
+++ autogen.sh
@@ -17,23 +17,23 @@ cd $srcdir
 }
 
 if automake-1.10 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.10
-    ACLOCAL=aclocal-1.10
+	AUTOMAKE=automake-1.10
+	ACLOCAL=aclocal-1.10
 elif automake-1.9 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.9
-    ACLOCAL=aclocal-1.9
+	AUTOMAKE=automake-1.9
+	ACLOCAL=aclocal-1.9
 elif automake-1.8 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.8
-    ACLOCAL=aclocal-1.8
+	AUTOMAKE=automake-1.8
+	ACLOCAL=aclocal-1.8
 elif automake-1.7 --version < /dev/null > /dev/null 2>&1 ; then
-    AUTOMAKE=automake-1.7
-    ACLOCAL=aclocal-1.7
+	AUTOMAKE=automake-1.7
+	ACLOCAL=aclocal-1.7
 else
-        echo
-        echo "You must have automake 1.7.x installed to compile $PROJECT."
-        echo "Install the appropriate package for your distribution,"
-        echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
-        DIE=1
+	echo
+	echo "You must have automake 1.7.x installed to compile $PROJECT."
+	echo "Install the appropriate package for your distribution,"
+	echo "or get the source tarball at http://ftp.gnu.org/gnu/automake/"
+	DIE=1
 fi
 
 (libtool --version) < /dev/null > /dev/null 2>&1 || {
@@ -49,15 +49,13 @@ fi
 	echo "You must have intltool installed to compile murrine."
 	DIE=1
 }
-                        
 if test "$DIE" -eq 1; then
 	exit 1
 fi
 
-
 if test -z "$*"; then
 	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
+	echo "to pass any to it, please specify them on the $0 command line."
 fi
 
 libtoolize --force --copy
@@ -65,7 +63,8 @@ intltoolize --force --copy --automake
 
 $ACLOCAL $ACLOCAL_FLAGS
 autoconf
+autoheader
 $AUTOMAKE --add-missing
 cd $THEDIR
 
-$srcdir/configure --enable-maintainer-mode "$@"
+$srcdir/configure "$@"

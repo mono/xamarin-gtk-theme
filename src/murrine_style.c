1295edd8cca9066048115e393515b7f96482be1a
Andrea <andrea.cimitan@gmail.com>
2010-03-30 22:56:24 +0200

    Fix gtk_paint_layout with widget == NULL

--- src/murrine_style.c
+++ src/murrine_style.c
@@ -1940,7 +1940,7 @@ murrine_style_draw_layout (GtkStyle     *style,
 	if (area)
 		gdk_gc_set_clip_rectangle (gc, area);
 
-	if (state_type == GTK_STATE_INSENSITIVE || 
+	if (widget && state_type == GTK_STATE_INSENSITIVE || 
 	    (&MURRINE_STYLE (style)->textstyle != 0 && state_type != GTK_STATE_PRELIGHT))
 	{
 		MurrineStyle *murrine_style = MURRINE_STYLE (style);

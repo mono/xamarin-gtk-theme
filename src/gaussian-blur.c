64377de11500ea96a86a75b8403c9469f8edd2a7
Cody Russell <bratsche@gnome.org>
2010-10-01 09:06:10 -0500

    Relicense Canonical's contributions from GPLv3 to dual-license LGPLv2.1 and LGPLv3.

--- src/gaussian-blur.c
+++ src/gaussian-blur.c
@@ -1,32 +1,27 @@
-////////////////////////////////////////////////////////////////////////////////
-//3456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789
-//      10        20        30        40        50        60        70        80
-//
-// raico-blur
-//
-// gaussian-blur.c - implements gaussian-blur function
-//
-// Copyright 2009 Canonical Ltd.
-//
-// Authors:
-//    Mirco "MacSlow" Mueller <mirco.mueller@canonical.com>
-//
-// Notes:
-//    based on filters in libpixman
-//
-// This program is free software: you can redistribute it and/or modify it
-// under the terms of the GNU General Public License version 3, as published
-// by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranties of
-// MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
-// PURPOSE.  See the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-////////////////////////////////////////////////////////////////////////////////
+/*
+ * Copyright 2009 Canonical Ltd.
+ *
+ * Authors:
+ *     Mirco "MacSlow" Mueller <mirco.mueller@canonical.com>
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of either or both of the following licenses:
+ *
+ * 1) the GNU Lesser General Public License version 3, as published by the
+ * Free Software Foundation; and/or
+ * 2) the GNU Lesser General Public License version 2.1, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the applicable version of the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of both the GNU Lesser General Public
+ * License version 3 and version 2.1 along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>
+ */
 
 #include <math.h>
 #include <pixman.h>
